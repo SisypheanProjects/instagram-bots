c8f38d09fa9abc7b9100d7c293678a0e9517e692
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2022-09-10 00:35:55 -0400

    Switch to more reliable Instagram Library (#6)

---
+++ app/Models/Instagram/Instagram.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+from typing import Dict, Union
+
+from instagrapi import Client
+
+
+class InstaGraphAPI:
+
+    __client: Client
+
+    def __init__(self, username: str, password: str):
+        self.__client = Client()
+        self.__client.login(username=username, password=password)
+
+    def photo_upload(self, file_path: str, caption: str, extra_data: Union[Dict, None] = None):
+        if extra_data is None:
+            extra_data = {}
+        self.__client.photo_upload(
+            path=Path(file_path),
+            caption=caption,
+            extra_data=extra_data
+        )
