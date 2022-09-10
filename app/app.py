5c00b68f7d915b5e34569e6ccb5143647178c393
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2022-09-09 20:42:42 -0400

    NASABot is Working! (#4)

--- app/app.py
+++ app/app.py
@@ -1,6 +1,7 @@
 import os
 from typing import List
 
+from AWS import CloudFormation
 from Models.Bots.IBot import IBot
 from Models.Bots.NASABot.NASABot import NASABot
 
@@ -8,8 +9,8 @@ try:
     INSTAGRAM_SECRET_ARN = os.environ["INSTAGRAM_SECRET_ARN"]
     SHARED_S3_BUCKET = os.environ["SHARED_S3_BUCKET"]
 except KeyError:
-    INSTAGRAM_SECRET_ARN = "<<INPUT>>>>"
-    SHARED_S3_BUCKET = "<<INPUT>>"
+    INSTAGRAM_SECRET_ARN = CloudFormation.get_stack_output("instagram-bots", "InstagramBotVariousSecrets")
+    SHARED_S3_BUCKET = CloudFormation.get_stack_output("instagram-bots", "InstagramSharedS3Bucket")
 
 bots: List[IBot] = [
     NASABot(INSTAGRAM_SECRET_ARN, SHARED_S3_BUCKET)
