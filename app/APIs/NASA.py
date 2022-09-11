689dea2fec371066fb2936a4d0037d68edc6d27d
Rayman Jamal <RaymanRJ@Live.com>
2022-09-10 21:28:10 -0400

    typing

--- app/APIs/NASA.py
+++ app/APIs/NASA.py
@@ -1,5 +1,5 @@
 from datetime import datetime
-from typing import Dict
+from typing import Dict, Union
 
 from Utilities.Utilities import get
 
@@ -7,7 +7,7 @@ __url = 'https://api.nasa.gov'
 __apod_path = '/planetary/apod'
 
 
-def apod_get(api_key: str, date: datetime = None) -> Dict:
+def apod_get(api_key: str, date: Union[datetime, None] = None) -> Dict:
     query_string = f'api_key={api_key}'
     if date is not None:
         query_string += f'&date={date.strftime("%Y-%m-%d")}'
