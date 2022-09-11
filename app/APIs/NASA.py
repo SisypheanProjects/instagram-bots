e5c33f31e9daaa9cba28bfd81e027a22523509ac
Rayman Jamal <34038866+RaymanRJ@users.noreply.github.com>
2022-09-10 21:31:25 -0400

    Search for a new NASA photo to post (#13)
    * Search for a new NASA photo to post
    
    * typing

--- app/APIs/NASA.py
+++ app/APIs/NASA.py
@@ -1,4 +1,5 @@
-from typing import Dict
+from datetime import datetime
+from typing import Dict, Union
 
 from Utilities.Utilities import get
 
@@ -6,6 +7,8 @@ __url = 'https://api.nasa.gov'
 __apod_path = '/planetary/apod'
 
 
-def apod_get(api_key: str) -> Dict:
+def apod_get(api_key: str, date: Union[datetime, None] = None) -> Dict:
     query_string = f'api_key={api_key}'
+    if date is not None:
+        query_string += f'&date={date.strftime("%Y-%m-%d")}'
     return get(__url, __apod_path, query_string)
